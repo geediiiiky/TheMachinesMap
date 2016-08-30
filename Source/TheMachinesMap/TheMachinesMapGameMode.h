27dc00b7eb6c55c014c5d9b4f4a7b9eb06a1798b
office-win-pc\shihai <wangshihai>
2016-08-30 18:08:33 +0800

    check in everything expect artlevel enlighten data which is more than 200M

---
+++ Source/TheMachinesMap/TheMachinesMapGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TheMachinesMapGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEMACHINESMAP_API ATheMachinesMapGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
